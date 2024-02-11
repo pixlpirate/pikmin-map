30a4a0841a38acef5a785f2ff7ee63300cd7fdf2
Jonathan Juste <jonathanc.juste@gmail.com>
2024-02-11 23:56:32 +0100

    Refactor makrdown loading
    Markdown now remaped to include anchors

--- src/app/components/templates/page-about/page-about.component.ts
+++ src/app/components/templates/page-about/page-about.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
-import { TranslateService } from '@ngx-translate/core';
-import { MarkdownComponent } from 'ngx-markdown';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { MarkdownComponent, MarkdownService } from 'ngx-markdown';
 
 import { LanguagePickerComponent } from '../../organisms/language-picker/language-picker.component';
 import { Subscription } from 'rxjs';
+import { MappingTableComponent } from '../../organisms/mapping-table/mapping-table.component';
 
 /**
  * The about page component used to display information about the project.
@@ -14,7 +15,7 @@ import { Subscription } from 'rxjs';
 @Component( {
 	selector: 'app-page-about',
 	standalone: true,
-	imports: [ CommonModule, LanguagePickerComponent, MarkdownComponent, RouterLink ],
+	imports: [ CommonModule, LanguagePickerComponent, MarkdownComponent, MappingTableComponent, RouterLink, TranslateModule ],
 	templateUrl: './page-about.component.html',
 	styleUrl: './page-about.component.scss'
 } )
@@ -22,12 +23,17 @@ export class PageAboutComponent implements OnInit, OnDestroy
 {
 	public currentLanguage: string = 'en';
 	private languageSubscription?: Subscription;
+	private hasScrolledToAnchor = false;
 
-	public aboutLoaded: boolean = false;
-	public faqLoaded: boolean = false;
-	public changelogLoaded: boolean = false;
+	public content: any = {
+		about: false,
+		faq: false,
+		changelog: false
+	};
 
 	constructor (
+		private router: Router,
+		private markdownService: MarkdownService,
 		private translateService: TranslateService
 	) { }
 
@@ -38,11 +44,30 @@ export class PageAboutComponent implements OnInit, OnDestroy
 		this.currentLanguage = this.translateService.currentLang;
 		this.languageSubscription = this.translateService.onLangChange.subscribe( ( event ) =>
 		{
+			// Reset the content
+			Object.keys( this.content ).forEach( key => this.content[ key ] = false );
+			this.hasScrolledToAnchor = false;
+
+			// Update the current language
 			this.currentLanguage = event.lang;
-			this.aboutLoaded = false;
-			this.faqLoaded = false;
-			this.changelogLoaded = false;
 		} );
+
+		// Remap the headings to include anchor links
+		this.markdownService.renderer.heading = ( text: string, level: number, raw: string ) =>
+		{
+			// generate id from text
+			let id = this.generateId( raw );
+
+
+			// Get current route and remove the anchor
+			const currentRoute = this.router.url.split( '#' )[ 0 ];
+
+			return '<h' + level + ' id="' + id + '" class="anchor-title">' +
+				'<a class="anchor" href="' + currentRoute + '#' + id + '" aria-label="' + this.translateService.instant( 'ANCHOR_TO' ) + ': ' + raw + '">' +
+				'<i class="icon icon-link"></i>' +
+				'</a>' + text +
+				'</h' + level + '>';
+		};
 	}
 
 	public ngOnDestroy()
@@ -50,24 +75,48 @@ export class PageAboutComponent implements OnInit, OnDestroy
 		this.languageSubscription?.unsubscribe();
 	}
 
-	/* Markdown
+	/* Navigation
 	--------------------------------------------- */
-	public onMarkdownLoad( event: string )
+
+	public navigateTo( path: string )
 	{
-		console.log( 'Markdown loaded', event );
+		this.router.navigate( [ path ], { replaceUrl: true } );
 	}
 
-	/* Translation
+	/* Markdown
 	--------------------------------------------- */
-	/**
-	 * Translate a key
-	 *
-	 * @param {string} key - The key to translate
-	 *
-	 * @returns {string} - The plain text translation associated with the key
-	 */
-	public translate( key: string ): string
+	public scrollToAnchor()
 	{
-		return this.translateService.instant( key );
+		const anchor = window.location.hash;
+		if ( anchor ) {
+			const element = document.querySelector( anchor );
+			if ( element ) {
+				element.scrollIntoView( { behavior: "smooth" } );
+			}
+		}
+	}
+
+	public allContentLoaded(): boolean
+	{
+		const loaded = Object.values( this.content ).every( value => value === true );
+
+		if ( loaded && !this.hasScrolledToAnchor ) {
+			this.scrollToAnchor();
+			this.hasScrolledToAnchor = true;
+		}
+
+		return loaded;
+	}
+
+	public generateId( text: string ): string
+	{
+		return text
+			.replace( /<[^>]*>/g, "" )				// remove html tags
+			.normalize( "NFD" )						// normalize diacritics
+			.replace( /[\u0300-\u036f]/g, "" )		// remove diacritics
+			.replace( /[^a-z0-9 ]/gi, '' )			// remove non alphanumeric or space characters
+			.trim()									// remove leading and trailing spaces
+			.toLowerCase()							// convert to lowercase
+			.replace( / /g, '-' );					// replace spaces with
 	}
 }
